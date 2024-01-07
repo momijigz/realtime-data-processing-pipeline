83201254c22525382e94037511dbe4bbe73d5ed2
Moazzam Khan <momijigz@gmail.com>
2024-01-07 20:26:24 +0500

    Testing out go

--- client/main.go
+++ client/main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v6"
+)
+
+type Person struct {
+	Age    int
+	Gender string
+}
+
+type Product struct {
+	Name     string
+	Brand    string
+	Category string
+	Price    float64
+}
+type Transaction struct {
+	ID              string
+	Person          Person
+	SelectedProduct Product
+	numbah          int
+}
+
+func (p *Person) SetGender() {
+	rand.Seed(time.Now().UnixNano())
+	gendah := []string{"male", "female"}
+	randomIndex := rand.Intn(len(gendah))
+	p.Age = 50
+	p.Gender = gendah[randomIndex]
+}
+
+func setGen() int {
+	return 1
+}
+
+func (t *Transaction) SetUUID() {
+	t.ID = gofakeit.UUID()
+}
+
+func GetRandomProduct(products []Product) (Product, error) {
+	if len(products) == 0 {
+		return Product{}, fmt.Errorf("empty product list")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	randomIndex := rand.Intn(len(products))
+	return products[randomIndex], nil
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	// Set the random seed for consistent data generation
+
+	// Array of structs
+	// products := []Product{
+	// 	{Category: "sports", Brand: "Nike", Price: 25, Name: "Nike Shoes"},
+	// 	{Category: "sports", Brand: "Adidas", Price: 50, Name: "Adidas Large Cap"},
+	// 	{Category: "sports", Brand: "Puma", Price: 75, Name: "Shoes"},
+	// 	{Category: "sports", Brand: "Sporta", Price: 25, Name: "Geep bunba"},
+	// 	{Category: "sports", Brand: "Geepa", Price: 100, Name: "Gopi bahu"},
+	// }
+	bobin := &Transaction{}
+	bobin.SetUUID()
+	bobin.Person.SetGender()
+
+	// Get a random product
+	randomProduct, err := GetRandomProduct(products)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bobin.SelectedProduct = randomProduct
+
+	fmt.Println(bobin)
 }
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/momijigz/realtime-data-processing-pipeline/server
+
+go 1.21.3
